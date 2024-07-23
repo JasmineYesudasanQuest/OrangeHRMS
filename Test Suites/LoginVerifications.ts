1661ada04724383b0e6a806f12513fab8eebf240
JasmineYesudasanQuest <jasmineyesudasan100@gmail.com>
2024-07-23 11:27:07 +0530

    Created new project structure

---
+++ Test Suites/LoginVerifications.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginVerifications</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0bad031c-7eb3-476e-9ac8-363f972636b1</testSuiteGuid>
+</TestSuiteEntity>
